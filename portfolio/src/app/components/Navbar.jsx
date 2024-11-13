e2ef48204f695723bc2250a22bdaf8f0b983fbe5
josie-opondo <josephineopondo5@gmail.com>
2024-11-13 10:18:05 +0300

    feat: added navbar with dynamic navlinks

--- portfolio/src/app/components/Navbar.jsx
+++ portfolio/src/app/components/Navbar.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 import Link from 'next/link'
+import NavLink from './NavLink'
+
+const navLinks = [
+    {
+        title: 'About',
+        path: '#about',
+    },
+    {
+        title: 'Projects',
+        path: '#projects',
+    },
+    {
+        title: 'Contact',
+        path: '#contact',
+    }
+]
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-            <Link href={"/"} className='text-5xl text-white font-semibold'>LOGO</Link>
-            <div className='menu hidden md:block md:w-auto' id='navbar'>
-                <ul>
-                    <li>
-                        <Link href={"#about"} className='block pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:-p-0 hover:text-white'>About</Link>
-                    </li>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+        <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
+            <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
+            <div className='menu block md:w-auto' id='navbar'>
+                <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                    {
+                        navLinks.map(({ title, path }) => (
+                            <li key={title}>
+                                <NavLink href={path} title={title} />
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
