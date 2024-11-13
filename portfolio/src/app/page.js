e79f32c697b0221426e099f3f38242555d62b809
josie-opondo <josephineopondo5@gmail.com>
2024-11-13 08:44:14 +0300

    chore: containerized hero section and cleaned up styling(buttons)

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col container mx-auto px-12 py-4">
-        <HeroSection />
+      <main className="flex min-h-screen flex-col">
+        <div class="container mx-auto px-12 py-4">
+          <HeroSection />
+        </div>
       </main>
   );
 }
