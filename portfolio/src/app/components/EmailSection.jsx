9bfff427b6139496b93fa9531beabe16c64d0ace
josie-opondo <josephineopondo5@gmail.com>
2024-11-14 18:56:58 +0300

    chore: add styling to the purple mesh

--- portfolio/src/app/components/EmailSection.jsx
+++ portfolio/src/app/components/EmailSection.jsx
@@ -44,7 +44,7 @@ const EmailSection = () => {
       id="contact"
       className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative"
     >
-      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
+      <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 md:-left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
           Let&apos;s Connect
@@ -123,6 +123,13 @@ const EmailSection = () => {
             >
               Send Message
             </button>
+            {
+              emailSubmitted && (
+                <p className="text-green-500 text-sm mt-2">
+                  Email sent successfully!
+                </p>
+              )
+            }
           </form>
         )}
       </div>
