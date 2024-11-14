69a336f8074904043144aecfed3f3ebee3e3124f
josie-opondo <josephineopondo5@gmail.com>
2024-11-14 21:39:41 +0300

    fix: formatting  for deployment

--- portfolio/src/app/components/HeroSection.jsx
+++ portfolio/src/app/components/HeroSection.jsx
@@ -17,41 +17,43 @@ const HeroSection = () => {
             transition={{ duration: 0.5 }}
             className="col-span-7 place-self-center text-center sm:text-left">
                 <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">Hello, I&apos;m{""}
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                      Hello, I&apos;m
                     </span>
                     <br />
                     <FlipWords
-      words={words}
-      wrapper="span"
-      speed={50}
-      repeat={Infinity}
-    />
+                      words={words}
+                      wrapper="span"
+                      speed={50}
+                      repeat={Infinity}
+                    />
                 </h1>
-                <p className='text-[#ADB7BE] text-base  sm:text-lg mb-6 lg:text-xl'>
-                Passionate about building seamless digital experiences with a blend of creativity and code.
+                <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
+                  Passionate about building seamless digital experiences with a blend of creativity and code.
                 </p>
                 <div>
-                <Link
-              href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
-            >
-              Hire Me
-            </Link>
-            <Link
-              href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
-            >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
-              </span>
-            </Link>
+                  <Link
+                    href="/#contact"
+                    className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+                  >
+                    Hire Me
+                  </Link>
+                  <Link
+                    href="/"
+                    className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3"
+                  >
+                    <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                      Download CV
+                    </span>
+                  </Link>
                 </div>
             </motion.div>
             <motion.div
-            initial={{ opacity: 0, scale: 0.5 }}
-            animate={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.5 }}
-            className="col-span-5 place-self-center mt-4 lg:mt-0">
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 0.5 }}
+              className="col-span-5 place-self-center mt-4 lg:mt-0"
+            >
                 <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
                     <Image
                         src='/images/hero-image.png'
